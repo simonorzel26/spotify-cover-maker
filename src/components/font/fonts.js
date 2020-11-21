99de263d0c30f71731600e958a19faed7c060380
bosco-reed <boscoe@inbox.lv>
2020-11-21 14:52:13 +0100

    first commit

---
+++ src/components/font/fonts.js
@@ -0,0 +1,14 @@
+const fonts = {
+	default: [
+		{ name: 'Arial', type: 'sans-serif', ref: 'default' },
+		{ name: 'Arial Black', type: 'sans-serif', ref: 'default' },
+		{ name: 'Spotify Font', type: 'sans-serif', ref: 'default' },
+	],
+	google: [],
+};
+
+export default {
+	getFonts() {
+		return fonts;
+	},
+};
